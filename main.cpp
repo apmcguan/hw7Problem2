3782e73c47b9a0016bdba746f33e7851054f8ecb
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-10-09 22:40:12 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+void time(int hours, int min, int secs);
+
+
+int main() {
+int s;
+
+cout<<" Enter the number of secounds that need to be converted :"<< endl;
+  cin>> s;
+  
+}
+void time(int hours, int min, int secs)
+{
+  
+}
