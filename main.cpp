dcf312d5bb3b8ad4f345ae54e71850296162f11d
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-10-13 02:29:54 +0000

    fixed function and working code

--- main.cpp
+++ main.cpp
@@ -2,23 +2,25 @@
 #include <cmath>
 
 using namespace std;
-void time(int hours, int min, int secs);
+void time(int hours, int min, int secs, int secounds);
 
 
 int main() {
 int hours, min, secs, secounds;
+  time( hours, min, secs, secounds);
   
-time ( hours, min, secs, secounds);
+
 
   
 }
 void time(int hours, int min, int secs, int secounds)
 {
+  cout<<"Enter value of secounds"<<endl;
   cin>>secounds;
   hours = (secounds/3600) % 60;
   min = (secounds / 60) % 60;
   secs = secounds % 60;
 
-  cout << hours<<"Hrs"<<min<<" mins "<<secs<<" secounds "<<endl;
+  cout << hours<<" Hrs "<<min<<" mins "<<secs<<" secounds "<<endl;
   
 }
